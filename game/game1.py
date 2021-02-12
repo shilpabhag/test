ff3d39de4c4c58ca1041500303641d026ec7154b
shbhag <72694928+shbhag@users.noreply.github.com>
2021-02-12 18:13:57 +0900

    game

---
+++ game/game1.py
@@ -0,0 +1,18 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("pong by @Abhinav")
+wn.bgcolor("red")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+
+
+
+
+# Main game loop
+while True:
+    wn.update()
+    
